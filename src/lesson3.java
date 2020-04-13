6928434aaacddad9816ad1fd7673d1c910cc96fb
Serhii Popelyshyn <serjukzol@gmail.com>
2020-04-13 21:13:27 +0100

    *

--- src/lesson3.java
+++ src/lesson3.java
@@ -1,2 +1,22 @@
+import com.sun.jndi.ldap.sasl.LdapSasl;
+
 public class lesson3 {
+    udsfiusagfsa
+    asf
+            dg
+    sdag
+                    ds
+    g
+                            sd
+    g
+                                    s
+    dg
+
+    public void setDg(s dg) {
+        LdapSaslgsda
+                g
+                sa
+                        dg
+                        ag
+    }
 }

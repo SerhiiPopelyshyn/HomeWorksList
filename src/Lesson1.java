5fb1fccac883894a32af02565b3623fe8f4aef2f
Serhii Popelyshyn <serjukzol@gmail.com>
2020-04-13 21:11:04 +0100

    Initial commit

---
+++ src/Lesson1.java
@@ -0,0 +1,48 @@
+public class Lesson1 {
+    public static class HW_1 {
+        public static void main(String[] args) {
+            //Primitive data types
+            byte q = 99;
+            short w = 19895;
+            int i = 10;
+            long e = 546456L;
+            float r = 15.99f;
+            double t = 12.21;
+            char y = 'Y';
+            boolean val = true;
+        }
+        //task 3 Counting with return value
+        public static int numbers(int a, int b, int c, int d) {
+            return a * (b + (c / d));
+        }
+        //task 4 in frames
+        public static void inFrames(int num1, int num2) {
+            if (num1 + num2 >= 10) && (num1 + num2 <= 20) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        }
+        //task 5 true or false
+        public static boolean guess(int a) {
+            int a = 5;
+            if (a > 0 || a == 0) {
+                System.out.println("positive");
+            } else {
+                System.out.println("negative");
+            }
+        }
+        //task 7 Hello
+        String name = "Serhii";
+        public static void hello(String name) {
+            System.out.println("Привет, " + name + "!";);
+        }
+        //task 8 Vusokostnuy
+        public static void year(String[] args) {
+            int year = 1995;
+            if (((year % 4 == 0) && (year % 100 !0)) || (0 == year % 400)){
+                System.out.println("Год" + year + "Bисокосный");
+            }
+        }
+    }
+}

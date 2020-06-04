23935384de4b16a93dc3e0052b7cc8a032fdd56a
Serhii Popelyshyn <serjukzol@gmail.com>
2020-06-04 19:01:47 +0100

    *

---
+++ src/JC_Lesson4/Calculator/NumButton.java
@@ -0,0 +1,15 @@
+package JC_Lesson4.Calculator;
+
+import javafx.scene.control.Button;
+
+public class NumButton extends Button {
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
